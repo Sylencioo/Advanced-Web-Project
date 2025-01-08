0f4329626235db7adab771dff0b55afceebd752d
Sylencioo <sylencioo@gmail.com>
2025-01-08 11:14:26 +0800

    Enhcance Drop Down Profile name in Header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -15,7 +15,7 @@
                     <ul>
                         <li class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle user-name" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }} <i class="bi bi-chevron-down"></i>
+                                {{ Auth::user()->name }} 
                             </a>
 
                             <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
